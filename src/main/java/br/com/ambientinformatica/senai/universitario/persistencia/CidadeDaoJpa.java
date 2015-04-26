0c8e245fd0560d43e01f61491714a6da22120e6b
gustavo.simoes <gustavo.simoes@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-04-26 22:50:17 +0000

    Inclusão das cidades na tabela cidade, na inicialização do sistema.

--- src/main/java/br/com/ambientinformatica/senai/universitario/persistencia/CidadeDaoJpa.java
+++ src/main/java/br/com/ambientinformatica/senai/universitario/persistencia/CidadeDaoJpa.java
@@ -1,14 +1,18 @@
 package br.com.ambientinformatica.senai.universitario.persistencia;
 
+import java.beans.Statement;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 import br.com.ambientinformatica.senai.universitario.entidade.Cidade;
@@ -17,6 +21,8 @@ import br.com.ambientinformatica.senai.universitario.entidade.Cidade;
 public class CidadeDaoJpa extends PersistenciaJpa<Cidade> implements CidadeDao {
 
 	private static final long serialVersionUID = 1L;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	public Cidade consultar(String nome, String uf) {
@@ -36,21 +42,26 @@ public class CidadeDaoJpa extends PersistenciaJpa<Cidade> implements CidadeDao {
 
 	@Override
 	public Cidade consultarPorNome(String nome) {
-		 Query q = this.em.createQuery("select c from Cidade c where c.nome=:nome");
-		   q.setParameter("nome", nome);
-		   return (Cidade)q.getSingleResult();
+		Query q = this.em.createQuery("select c from Cidade c where c.nome=:nome");
+		q.setParameter("nome", nome);
+		return (Cidade)q.getSingleResult();
 	}
 
 	@Override
 	@SuppressWarnings("resource")
+	@Transactional
 	public void incluirViaArquivo(String caminho) throws IOException {
 		File arquivoSql = new File(caminho);
 		FileReader fr = new FileReader(arquivoSql);
 		BufferedReader br = new BufferedReader(fr);
 		String linha = br.readLine();  
 		while( linha != null ){
+			if (linha.contains("ï»¿")){
+				linha = linha.replaceAll("ï»¿", "");
+			}
 			Query query = em.createNativeQuery(linha);
 			query.executeUpdate();
+			linha = br.readLine();
 		}
 	}
 }
