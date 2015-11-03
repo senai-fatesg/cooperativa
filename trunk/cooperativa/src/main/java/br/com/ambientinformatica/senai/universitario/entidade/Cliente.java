1da6debd40fe669975c0e378ce9ee227d22328c4
carloshsoft <carloshsoft@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-11-03 17:53:18 +0000

    Entidade Cliente.

---
+++ trunk/cooperativa/src/main/java/br/com/ambientinformatica/senai/universitario/entidade/Cliente.java
@@ -0,0 +1,105 @@
+package br.com.ambientinformatica.senai.universitario.entidade;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(generator = "cliente_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "cliente_seq", sequenceName = "cliente_seq", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	private String nome;
+	private String cpfCnpj;
+	private String telefone01;
+	private String telefone02;
+	private String status;
+	@Temporal(TemporalType.DATE)
+	private Date dtExclusao;
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Endereco endereco = new Endereco();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public String getTelefone01() {
+		return telefone01;
+	}
+
+	public void setTelefone01(String telefone01) {
+		this.telefone01 = telefone01;
+	}
+
+	public String getTelefone02() {
+		return telefone02;
+	}
+
+	public void setTelefone02(String telefone02) {
+		this.telefone02 = telefone02;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getDtExclusao() {
+		return dtExclusao;
+	}
+
+	public void setDtExclusao(Date dtExclusao) {
+		this.dtExclusao = dtExclusao;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public String toString() {
+		if (this.getNome() != null) {
+			return this.getId() + " - " + this.getNome();
+		}
+		return this.getId().toString();
+	}
+
+}
